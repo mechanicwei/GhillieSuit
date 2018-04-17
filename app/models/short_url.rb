1f6be31dbce107f6a7ee7c66fea4d583b5e0a3f0
John Wei <676018683@qq.com>
2018-04-17 21:35:51 +0800

    set validations for short_url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,2 +1,6 @@
 class ShortUrl < ApplicationRecord
+  validates :destination, presence: true
+  validates :key, presence: true, uniqueness: true
+
+  store_accessor :limit_settings, :length, :char_set, :custom_key
 end
