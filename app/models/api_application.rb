660918b28292da408b6e6b287106c3abb4e2a9f6
John Wei <676018683@qq.com>
2018-04-18 10:16:18 +0800

    set private_token for api_application

--- app/models/api_application.rb
+++ app/models/api_application.rb
@@ -1,3 +1,18 @@
 class ApiApplication < ApplicationRecord
   has_many :short_urls, dependent: :destroy
+
+  before_create :set_private_token
+
+  private
+
+  def set_private_token
+    self.private_token ||= generate_token
+  end
+
+  def generate_token
+    begin
+      token = SecureRandom.urlsafe_base64(24).tr('-_=', 'fAh')
+    end while self.class.exists?(private_token: token)
+    token
+  end
 end
--- spec/models/api_application_spec.rb
+++ spec/models/api_application_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ApiApplication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#set_private_token' do
+    it 'set a private_token on create' do
+      api_application = Fabricate :api_application, private_token: nil
+      expect(api_application.private_token.present?).to eq true
+    end
+  end
 end
